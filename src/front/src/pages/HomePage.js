cf4cf6cef11a33fae2b47f0449737d2b36ed1eba
mladen081 <jovicmladen094@gmail.com>
2022-05-29 17:52:29 +0200

    Started HomePage.

---
+++ src/front/src/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Layout from "../components/Layout";
+import { Footer } from "../components/Footer";
+
+export const HomePage = () => {
+  return (
+    <div className="HomePage">
+      <Layout />
+      <Footer />
+    </div>
+  );
+}
